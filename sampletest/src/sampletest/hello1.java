2e461945e0e99bad3498c83757b067e4b05c49b2
lakshmineha <110032826+lakshmineha@users.noreply.github.com>
2023-12-21 15:19:19 +0530

    Update hello1.java

--- sampletest/src/sampletest/hello1.java
+++ sampletest/src/sampletest/hello1.java
@@ -2,7 +2,7 @@ package sampletest;
 
 public class hello1 {
 	public static void main(String[] args) {
-		System.out.println("hellotestingteam");
+		System.out.println("hellotestingteam1234");
 		
 	}
 
